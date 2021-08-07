8c061efa2c436504eda87ec1abb49980b89b0c6d
Damien <damien.kemp@students.cau.edu>
2021-08-07 16:22:44 -0400

    refactored ExpenseItem component also added ExpenseDate component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expense = [
+    {
+      title: 'Rent',
+      amount: 1317.94,
+      date: new Date(2021, 8, 1)
+    },
+    {
+      title: 'Car Note',
+      amount: 377.18,
+      date: new Date(2021, 8, 28)
+    },
+    {
+      title: 'Phone Bill',
+      amount: 133.00,
+      date: new Date(2021, 8, 1)
+    }
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}>
+      </ExpenseItem>
     </div>
   );
 }
